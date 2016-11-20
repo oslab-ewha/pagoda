1ce4427dce758ee40c4acd21576ec814793b9ced
yeh14 <yeh14@purdue.edu>
2016-11-20 14:04:12 -0500

    update beamformer static fusion

--- beamformer/cuda_fused/cuda_beamformer.cu
+++ beamformer/cuda_fused/cuda_beamformer.cu
@@ -148,7 +148,7 @@ int main(){
   	}
   	checkCudaErrors(cudaDeviceSynchronize());
   	end_timer = my_timer();
-  	printf("Beamformer pagoda elapsed time:%lf sec.\n", end_timer - start_timer);
+  	printf("Beamformer CUDA static fusion elapsed time:%lf sec.\n", end_timer - start_timer);
 
   	// copy back
   	start_timer = my_timer();
